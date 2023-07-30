ee5fd1963b61641a5fc6bdc192aba15a217a6321
Adriel Klem <adrielklem16@gmail.com>
2023-07-30 16:22:34 -0300

    update event

--- Calculadora IMC/js/scripts.js
+++ Calculadora IMC/js/scripts.js
@@ -2,16 +2,22 @@
 const form = document.querySelector('form')
 const inputWeight = document.querySelector("#weight")
 const inputHeight = document.querySelector("#height")
-
+const modalWrapper = document.querySelector(".modal-wrapper")
+const modalMessage = document.querySelector(".modal .title span")
+const modalBtnClose = document.querySelector(".modal button.close")
 
 form.onsubmit = (event) => {
     event.preventDefault()
 
     const weight = inputWeight.value
     const height = inputHeight.value
+    
+    modalMessage.innerHTML = `Seu IMC é os ${IMC(weight,height)}`
 
-    const result = IMC(weight,height)
+    modalWrapper.classList.add('open')
 }
+
+modalBtnClose.onclick = () => modalWrapper.classList.remove('open')
 
 function IMC(weight, height) {
     return (weight /((height / 100)**2)).toFixed(2)
