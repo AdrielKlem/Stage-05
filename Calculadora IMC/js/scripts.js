8b389fca74206543d90b605a70a6e9095cc665e5
Adriel Klem <adrielklem16@gmail.com>
2023-07-30 15:54:46 -0300

    add event js

---
+++ Calculadora IMC/js/scripts.js
@@ -0,0 +1,8 @@
+// var global
+const form = document.querySelector('form')
+const inputWeight = document.querySelector("#weight")
+const inputHeight = document.querySelector("#height")
+
+
+// 3 maneiras de criar e atribuir função a um evento
+form.onsubmit = () => {}
