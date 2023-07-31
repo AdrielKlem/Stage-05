1d17c1bb96c6ac8eea8e31d6a7777e78caed1e73
Adriel Klem <adrielklem16@gmail.com>
2023-07-31 16:53:26 -0300

    add modal js

---
+++ Calculadora IMC/js/modal.js
@@ -0,0 +1,13 @@
+const Modal = {
+    open() {
+    modalWrapper.classList.add('open')
+        
+    }, 
+    //open: function() {},
+    close() {
+        modalWrapper.classList.remove('open')
+    }
+    //close: function() {},
+}
+
+modalBtnClose.onclick = () => Modal.close()
